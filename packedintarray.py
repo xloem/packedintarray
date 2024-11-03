a6e29cc19877c913732672aacf1454a6ae86ddf5
John Doe <johndoe@example.com>
2024-11-02 23:07:21 -0600

    added dense slice getter, no slice setter yet

--- packedintarray.py
+++ packedintarray.py
@@ -8,32 +8,44 @@ class PackedIntArray:
 
     If an integer is passed rather than a storage object, a new bytearray is allocated of sufficient size to hold that many bitwidths.
     '''
-    def __init__(self, bitwidth, storage, endian = 'little'):
+    def __init__(self, bitwidth, storage, endian = 'little', bitoffset = 0, max_len = None):
         assert endian in ['little', 'big']
         if type(storage) is int:
-            storage = bytearray((storage * bitwidth - 1) / 8 + 1) # ceil(x / y) == (x - 1) // y + 1
+            storage = bytearray((storage * bitwidth - 1) // 8 + 1) # ceil(x / y) == (x - 1) // y + 1
         self.bitwidth = bitwidth
+        self.bitoffset = bitoffset
+        self.max_len = max_len
         self.storage = storage
         self.get_range_shift = getattr(self, '_get_range_shift_' + endian)
         self.value_mask = (1 << bitwidth) - 1
         self.endian = endian
     def __len__(self):
-        return len(self.storage) * 8 // self.bitwidth
-    def _get_range_shift_big(self, index):
+        length = len(self.storage) * 8 // self.bitwidth
+        if self.max_len is not None:
+            return min(self.max_len, length)
+        else:
+            return length
+    def _get_range_shift_big(self, index, count=1):
         bitwidth = self.bitwidth
-        bits = index * bitwidth
+        bits = index * bitwidth + self.bitoffset
         offset1 = bits // 8
-        offset2, bits2 = divmod(bits+bitwidth+8, 8)
-        return [offset1, offset2, 8-bits2]
-    def _get_range_shift_little(self, index):
+        offset2, bits2 = divmod(bits + bitwidth * count + 8, 8)
+        return [offset1, offset2, 8 - bits2]
+    def _get_range_shift_little(self, index, count=1):
         bitwidth = self.bitwidth
-        bits = index * bitwidth
+        bits = index * bitwidth + self.bitoffset
         offset1, bits1 = divmod(bits, 8)
-        offset2 = (bits + bitwidth + 8) // 8
+        offset2 = (bits + bitwidth * count + 8) // 8
         return [offset1, offset2, bits1]
     def __getitem__(self, index):
-        start, end, shift = self.get_range_shift(index)
-        return (int.from_bytes(self.storage[start:end], self.endian) >> shift) & self.value_mask
+        if type(index) is slice:
+            assert index.step in [None,1] # todo if desired
+            count = index.stop - index.start
+            start, end, shift = self.get_range_shift(index.start, count)
+            return type(self)(self.bitwidth, self.storage[start:end], self.endian, shift, count)
+        else:
+            start, end, shift = self.get_range_shift(index)
+            return (int.from_bytes(self.storage[start:end], self.endian) >> shift) & self.value_mask
     def __setitem__(self, index, value):
         start, end, shift = self.get_range_shift(index)
         data = (int.from_bytes(self.storage[start:end], self.endian) & ~(self.value_mask << shift)) | (value << shift)
@@ -43,6 +55,7 @@ class PackedIntArray:
             yield self[idx]
 
 def _test():
+    import tqdm
     for bitwidth in [3, 9]:
         for endian in ['little', 'big']:
             ints = list(range(1<<bitwidth))
@@ -52,6 +65,12 @@ def _test():
                 testarray[idx] = ints[idx]
             for idx in range(len(ints)):
                 assert testarray[idx] == ints[idx]
+            for start in tqdm.tqdm(range(len(ints)), desc=f'all {bitwidth}bit {endian} slices'):
+                for stop in range(start, len(ints)):
+                    subarray = testarray[start:stop]
+                    subints = ints[start:stop]
+                    for idx in range(len(subints)):
+                        assert subarray[idx] == subints[idx]
 
 if __name__ == '__main__':
     _test()
